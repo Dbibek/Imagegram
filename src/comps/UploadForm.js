5ea4c0ac5d423ced8ecbbf1a9bebf897036b0d96
Bibek Dhakal <47821826+Dbibek@users.noreply.github.com>
2021-01-12 00:15:56 +0200

    upload form and filetype check added

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [err, setErr] = useState(null);
+
+  const types = ["image/png", "image/jpeg"];
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    let selected = e.target.files[0];
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setErr("");
+    } else {
+      setFile(null);
+      setErr("please select and image file( png or jpeg)");
+    }
+  };
   return (
     <form>
-      <input type='file' />
+      <input type="file" onChange={handleChange} />
+      <div className="output">
+        {err && <div className="error">{err}</div>}
+        {file && <div>{file.name}</div>}
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default UploadForm
+export default UploadForm;
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,20 @@
-// import * as firebase from "firebase/app";
-// import "firebase/storage";
-// import "firebase/firestore";
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
 
-// // Your web app's Firebase configuration
-// var firebaseConfig = {
-//   apiKey: "AIzaSyCjIyeLZjK9zF0z-m-CSlQpWm1eBWvGd3c",
-//   authDomain: "imagegram-92765.firebaseapp.com",
-//   projectId: "imagegram-92765",
-//   storageBucket: "imagegram-92765.appspot.com",
-//   messagingSenderId: "670602284638",
-//   appId: "1:670602284638:web:6537cd59a8aefb2d566ecf",
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCjIyeLZjK9zF0z-m-CSlQpWm1eBWvGd3c",
+  authDomain: "imagegram-92765.firebaseapp.com",
+  projectId: "imagegram-92765",
+  storageBucket: "imagegram-92765.appspot.com",
+  messagingSenderId: "670602284638",
+  appId: "1:670602284638:web:6537cd59a8aefb2d566ecf",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// const projectStorage = firebase.storage();
-// const projectFirestore = firebase.firestore();
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
 
-// export { projectStorage, projectFirestore };
+export { projectStorage, projectFirestore };
