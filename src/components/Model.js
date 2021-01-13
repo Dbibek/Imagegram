fd459d225464d0ccddc603d5b3b62b10a594afec
Bibek Dhakal <47821826+Dbibek@users.noreply.github.com>
2021-01-13 23:23:49 +0200

    farmer motion animation added

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Model = ({ selected, setSelected }) => {
   const handleClick = (e) => {
     return e.target.classList.contains("backdrop") ? setSelected(null) : null;
   };
   return (
-    <div className="backdrop" onClick={handleClick}>
-      <img src={selected} alt="full size pic" />
-    </div>
+    <motion.div
+      className="backdrop"
+      onClick={handleClick}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      
+    >
+      <motion.img src={selected} alt="full size pic"
+      initial={{y:"-100vh"}}
+      animate={{y:0}} />
+    </motion.div>
   );
 };
 
