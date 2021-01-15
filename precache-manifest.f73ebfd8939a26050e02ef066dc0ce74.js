1e565353d5ebd0f403fe5fe96d1272c523452981
Bibek Dhakal <47821826+Dbibek@users.noreply.github.com>
2021-01-16 00:05:40 +0200

    Updates

---
+++ precache-manifest.f73ebfd8939a26050e02ef066dc0ce74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c8864a7dd6b1ef417087d16002683f",
+    "url": "/Imagegram/index.html"
+  },
+  {
+    "revision": "b593fe6ce24c234ddfc9",
+    "url": "/Imagegram/static/css/main.f4167500.chunk.css"
+  },
+  {
+    "revision": "c45fd1fac81d73e859e0",
+    "url": "/Imagegram/static/js/2.1f5c7908.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/Imagegram/static/js/2.1f5c7908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b593fe6ce24c234ddfc9",
+    "url": "/Imagegram/static/js/main.5ae867e3.chunk.js"
+  },
+  {
+    "revision": "d19aca35a398123958af",
+    "url": "/Imagegram/static/js/runtime-main.ab88e0d9.js"
+  }
+]);
