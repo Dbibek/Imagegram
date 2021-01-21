00424136b2dd861a4d4c9633922a6a538f2e0d38
Bibek Dhakal <47821826+Dbibek@users.noreply.github.com>
2021-01-21 23:51:42 +0200

    logout and update profile link added

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 const Title = () => {
+  const [error, setError] = useState("");
+  const { logout } = useAuth();
+  const history = useHistory();
+  async function handleClick() {
+    setError("");
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("failed to logout");
+    }
+  }
   return (
-    <div className="title">
-      <h1>ImageGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Photograph: a picture painted by the sun without instruction in art.</p>
-      <p>“You can look at a picture for a week and never think of it again. You can also look at a picture for a second and think of it all your life.”</p>
-    </div>
-  )
-}
+    <>
+      <div className="title">
+        <div className="d-flex justify-content-between w-100 text-center mt-2  ">
+          <h1>ImageGram</h1>
+          <div className="d-flex justify-content-between">
+            <Link to="/update-profile" variant="Link" className=" w-60 mt-2">
+              Update Profile
+            </Link>
+            <Button variant="Link" className="" onClick={handleClick}>
+              Log Out
+            </Button>
+          </div>
+        </div>
+        <h2>Your Pictures</h2>
+        <p>
+          Photograph: a picture painted by the sun without instruction in art.
+        </p>
+        <p>
+          “You can look at a picture for a week and never think of it again. You
+          can also look at a picture for a second and think of it all your
+          life.”
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default Title;
